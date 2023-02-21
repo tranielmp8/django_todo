3b9d890ef8a6777895b8f1dc562e1a72067c5084
Traniel Pride <tranielmp8@gmail.com>
2023-02-21 06:21:45 -0600

    new django todo app

---
+++ todo_app/urls.py
@@ -0,0 +1,23 @@
+# from asyncio import Task
+from django.urls import path
+from .views import (
+    TaskList,
+    TaskDetail,
+    TaskCreate,
+    TaskUpdate,
+    TaskDelete,
+    CustomLoginView,
+    RegisterPage
+)
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='tasks'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task_create/', TaskCreate.as_view(), name='task_create'),
+    path('task_update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
+    path('task_delete/<int:pk>/', TaskDelete.as_view(), name='task_delete')
+]
