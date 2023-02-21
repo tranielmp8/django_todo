d486386036ef809d96c9d9371ccf9568d0b887bf
Traniel Pride <tranielmp8@gmail.com>
2023-02-21 07:15:13 -0600

    adding accounts/ to url for login and register

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -13,9 +13,9 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('register/', RegisterPage.as_view(), name='register'),
+    path('accounts/register/', RegisterPage.as_view(), name='register'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task_create/', TaskCreate.as_view(), name='task_create'),
     path('task_update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
