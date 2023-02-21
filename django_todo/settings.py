0a42f04a6aa765097d5b7204dd3f8e327d6d19dd
Traniel Pride <tranielmp8@gmail.com>
2023-02-21 07:00:47 -0600

    allowed hosts update

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangotodo-production-5fdb.up.railway.app', '127.0.0.1']
 
 
 # Application definition
